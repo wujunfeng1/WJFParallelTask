a2ea674bdf18093cff1bacf58b886b1e4db9d4d7
Junfeng Wu <wujunfeng1@gmail.com>
2021-12-20 09:33:58 +1100

    simpify interface

--- src/WJFParallelTask.jl
+++ src/WJFParallelTask.jl
@@ -2,43 +2,44 @@ module WJFParallelTask
 export mapPrefix, mapReduce
 using Distributed
 
-@everywhere function mapPrefix(loopStart::T1, loopEnd::T1, batchSize::T1,
-    mapFun::Function, blockPrefixFun::Function,
-    data::T2,
+@everywhere function mapPrefix(data::T2, batchSize::Int,
+    mapFun::Function, blockPrefixFun::Function;
+    globalStates::Dict{String,Any} = Dict{String,Any}(),
     copyData::Bool = false,
-    )::T2 where {T1<:Integer, T2<:Vector}
+    )::T2 where {T2<:Vector}
     numWorkers = length(workers())
-    jobs = RemoteChannel(() -> Channel{Tuple{T1,T1,T2}}(numWorkers))
-    jobOutputs = RemoteChannel(() -> Channel{Vector{Tuple{T1,T1,T2}}}(numWorkers))
+    jobs = RemoteChannel(() -> Channel{Tuple{Int,Int,T2,Dict{String,Any}}}(numWorkers))
+    jobOutputs = RemoteChannel(() -> Channel{Vector{Tuple{Int,Int,T2}}}(numWorkers))
 
     function makeJobs()
-        for i::T1 = loopStart:batchSize:loopEnd
-            iEnd = min(i + batchSize - 1, loopEnd)
+        for i = 1:batchSize:length(data)
+            iEnd = min(i + batchSize - 1, length(data))
             if copyData
-                put!(jobs, (i, iEnd, data))
+                put!(jobs, (i, iEnd, data, globalStates))
             else
-                put!(jobs, (i, iEnd, data[i:iEnd]))
+                put!(jobs, (i, iEnd, data[i:iEnd], globalStates))
             end
         end
         for idxWorker = 1:numWorkers
-            put!(jobs, (loopEnd,loopEnd,T2()))
+            put!(jobs, (0,0,T2(),Dict{String,Any}()))
         end
     end
 
     errormonitor(@async makeJobs())
     for p in workers()
         remote_do((jobs, jobOutputs) -> begin
-            localResult::Vector{Tuple{T1,T1,T2}} = Vector{Tuple{T1,T1,T2}}()
+            localResult::Vector{Tuple{Int,Int,T2}} = Vector{Tuple{Int,Int,T2}}()
             job = take!(jobs)
             while length(job[3]) > 0
-                push!(localResult, (job[1], job[2], mapFun(job[1], job[2], job[3])))
+                push!(localResult, (job[1], job[2],
+                    mapFun(job[1], job[2], job[3], job[4])))
                 job = take!(jobs)
             end
             put!(jobOutputs, localResult)
         end, p, jobs, jobOutputs)
     end
 
-    localResults::Vector{Tuple{T1,T1,T2}} = Vector{Tuple{T1,T1,T2}}()
+    localResults::Vector{Tuple{Int,Int,T2}} = Vector{Tuple{Int,Int,T2}}()
     for idxWorker = 1:numWorkers
         append!(localResults, take!(jobOutputs))
     end
@@ -54,26 +55,26 @@ using Distributed
     return result
 end
 
-@everywhere function mapReduce(loopStart::T1, loopEnd::T1, batchSize::T1,
+@everywhere function mapReduce(data::T2, batchSize::Int,
     mapFun::Function, reduceFun::Function,
-    data::T2, outData0::T3,
+    outData0::T3; globalStates::Dict{String,Any} = Dict{String,Any}(),
     copyData::Bool = false,
-    )::T3 where {T1<:Integer, T2<:Vector, T3<:Any}
+    )::T3 where {T2<:Vector, T3<:Any}
     numWorkers = length(workers())
-    jobs = RemoteChannel(() -> Channel{Tuple{T1,T1,T2}}(numWorkers))
+    jobs = RemoteChannel(() -> Channel{Tuple{Int,Int,T2,Dict{String,Any}}}(numWorkers))
     jobOutputs = RemoteChannel(() -> Channel{T3}(numWorkers))
 
     function makeJobs()
-        for i::T1 = loopStart:batchSize:loopEnd
-            iEnd = min(i + batchSize - 1, loopEnd)
+        for i = 1:batchSize:length(data)
+            iEnd = min(i + batchSize - 1, length(data))
             if copyData
-                put!(jobs, (i, iEnd, data))
+                put!(jobs, (i, iEnd, data, globalStates))
             else
-                put!(jobs, (i, iEnd, data[i:iEnd]))
+                put!(jobs, (i, iEnd, data[i:iEnd], globalStates))
             end
         end
         for idxWorker = 1:numWorkers
-            put!(jobs, (loopEnd,loopEnd,T2()))
+            put!(jobs, (0,0,T2(),Dict{String,Any}()))
         end
     end
 
@@ -83,7 +84,7 @@ end
             localResult::Vector{T3} = Vector{T3}()
             job = take!(jobs)
             while length(job[3]) > 0
-                push!(localResult, mapFun(job[1], job[2], job[3]))
+                push!(localResult, mapFun(job[1], job[2], job[3], job[4]))
                 job = take!(jobs)
             end
             put!(jobOutputs, reduceFun(localResult))
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,14 @@ include("../src/WJFParallelTask.jl")
 
 @testset "map prefix" begin
     a = Float64[i for i = 1:4000000]
-    @everywhere function mapFun(i1, i2, segment::Vector{Float64})
+    @everywhere function mapFun(i1, i2, segment::Vector{Float64}, globalStates::Dict{String,Any})
         result = copy(segment)
         for j = 2:length(result)
             result[j] += result[j - 1]
         end
         return result
     end
-    @everywhere function mapFun1(i1, i2, data::Vector{Float64})
+    @everywhere function mapFun1(i1, i2, data::Vector{Float64}, globalStates::Dict{String,Any})
         result = copy(data[i1:i2])
         for j = 2:length(result)
             result[j] += result[j - 1]
@@ -24,10 +24,10 @@ include("../src/WJFParallelTask.jl")
         end
         return result
     end
-    @everywhere function mapFun2(i1, i2, segment::Vector{Float64})
+    @everywhere function mapFun2(i1, i2, segment::Vector{Float64}, globalStates::Dict{String,Any})
         return sum(segment)
     end
-    @everywhere function mapFun3(i1, i2, data::Vector{Float64})
+    @everywhere function mapFun3(i1, i2, data::Vector{Float64}, globalStates::Dict{String,Any})
         return sum(data[i1:i2])
     end
     function reduceFun(xs::Vector{Float64})
@@ -38,20 +38,14 @@ include("../src/WJFParallelTask.jl")
     for i = 2:length(a)
         aResult[i] = aResult[i-1] + a[i]
     end
-    @time b = mapPrefix(
-        1,length(a),1000, mapFun, blockPrefixFun, a)
+    @time b = mapPrefix(a, 1000, mapFun, blockPrefixFun)
     err = sum(abs.(aResult .- b))
     println("err = $err")
-    @time b1 = mapPrefix(
-        1,length(a),1000, mapFun1, blockPrefixFun, a, true)
+    @time b1 = mapPrefix(a, 1000, mapFun1, blockPrefixFun, copyData=true)
     err1 = sum(abs.(aResult .- b1))
     println("err1 = $err1")
-    @time c = mapReduce(
-        1,length(a),1000, mapFun2, reduceFun, a, 0.0
-    )
-    @time c1 = mapReduce(
-        1,length(a),1000, mapFun3, reduceFun, a, 0.0, true
-    )
+    @time c = mapReduce(a, 1000, mapFun2, reduceFun, 0.0)
+    @time c1 = mapReduce(a, 1000, mapFun3, reduceFun, 0.0, copyData=true)
     @test err ≈ 0
     @test err1 ≈ 0
     @test c ≈ aResult[end]
